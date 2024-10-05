c9fff31a71a106d23c4fda71b48857f83878e329
Daniel <daniel.soto48@inacapmail.cl>
2024-10-04 23:17:15 -0300

    fix reload components

--- Proyecto/src/app/app.module.ts
+++ Proyecto/src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { UserComponent } from './user/user.component';
-import { AdminComponent } from './admin/admin.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UserComponent } from './components/user/user.component';
+import { AdminComponent } from './components/admin/admin.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from './user/login/login.component';
+import { LoginComponent } from './components/user/login/login.component';
 
 @NgModule({
   declarations: [
